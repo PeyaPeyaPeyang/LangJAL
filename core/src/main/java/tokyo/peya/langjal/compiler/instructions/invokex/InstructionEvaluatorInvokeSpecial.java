6d8a155796f7ca62c163ffed21698c3d8af18cf4
PeyaPeyaPeyang <peyang@peya.tokyo>
2025-07-25 08:44:24 +0900

    fix: Missing evaluation of return type of invokespecial

--- core/src/main/java/tokyo/peya/langjal/compiler/instructions/invokex/InstructionEvaluatorInvokeSpecial.java
+++ core/src/main/java/tokyo/peya/langjal/compiler/instructions/invokex/InstructionEvaluatorInvokeSpecial.java
@@ -2,6 +2,7 @@ package tokyo.peya.langjal.compiler.instructions.invokex;
 
 import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 import tokyo.peya.langjal.compiler.JALParser;
 import tokyo.peya.langjal.analyser.FrameDifferenceInfo;
 import tokyo.peya.langjal.analyser.stack.NopElement;
@@ -11,6 +12,8 @@ import tokyo.peya.langjal.analyser.stack.UninitializedThisElement;
 import tokyo.peya.langjal.compiler.instructions.AbstractInstructionEvaluator;
 import tokyo.peya.langjal.compiler.jvm.EOpcodes;
 import tokyo.peya.langjal.compiler.jvm.MethodDescriptor;
+import tokyo.peya.langjal.compiler.jvm.PrimitiveTypes;
+import tokyo.peya.langjal.compiler.jvm.Type;
 import tokyo.peya.langjal.compiler.jvm.TypeDescriptor;
 import tokyo.peya.langjal.compiler.member.EvaluatedInstruction;
 import tokyo.peya.langjal.compiler.member.InstructionInfo;
@@ -44,7 +47,7 @@ public class InstructionEvaluatorInvokeSpecial
     {
         MethodInsnNode method = (MethodInsnNode) instruction.insn();
         MethodDescriptor descriptor = MethodDescriptor.parse(method.desc);
-        TypeDescriptor returnType = descriptor.getReturnType();
+        TypeDescriptor returnTypeDesc = descriptor.getReturnType();
         TypeDescriptor[] parameterTypes = descriptor.getParameterTypes();
 
         FrameDifferenceInfo.Builder builder = FrameDifferenceInfo.builder(instruction);
@@ -54,6 +57,20 @@ public class InstructionEvaluatorInvokeSpecial
                 parameterTypes
         );
 
+        opSpecialInvocation(instruction, builder, method);
+
+        // 戻り値の型に応じてスタックを調整
+        InstructionEvaluateHelperInvocation.opReturnType(
+                builder,
+                instruction,
+                returnTypeDesc
+        );
+        return builder.build();
+    }
+
+    private static void opSpecialInvocation(@NotNull InstructionInfo instruction, @NotNull FrameDifferenceInfo.Builder builder,
+                                     @NotNull MethodInsnNode method)
+    {
         StackElementCapsule uninitialisedRefCapsule = new StackElementCapsule(
                 instruction, actualElm -> {
             // UninitializedThisElement の場合は，ObjectElement をローカル変数０に入れる
@@ -73,8 +90,6 @@ public class InstructionEvaluatorInvokeSpecial
 
         if (method.name.equals("<init>") && method.owner.equals(instruction.ownerClass().superName))
             builder.addLocalFromCapsule(0, uninitialisedRefCapsule);
-
-        return builder.build();
     }
 
     @Override
