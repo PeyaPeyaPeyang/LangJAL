b3014679f17aee6f0f80c32ca2daa410b5b8d9bf
PeyaPeyaPeyang <peyang@peya.tokyo>
2026-05-09 05:04:24 +0900

    refactor: Enhance AbstractInstructionTestCase with parameterized tests

--- core/src/test/java/tokyo/peya/langjal/compiler/instructions/TestNop.java
+++ core/src/test/java/tokyo/peya/langjal/compiler/instructions/TestNop.java
@@ -1,12 +1,21 @@
 package tokyo.peya.langjal.compiler.instructions;
 
+import tokyo.peya.langjal.compiler.JALParser;
 import tokyo.peya.langjal.compiler.instructions.utils.AbstractInstructionTestCase;
 import tokyo.peya.langjal.compiler.jvm.EOpcodes;
 
-public class TestNop extends AbstractInstructionTestCase
+public class TestNop extends AbstractInstructionTestCase<JALParser.JvmInsNopContext, InstructionEvaluatorNop>
 {
     public TestNop()
     {
         super(new InstructionEvaluatorNop(), EOpcodes.NOP);
+    }
+
+    @Override
+    public String[] getValidInstructions()
+    {
+        return new String[] {
+                "nop",
+        };
     }
 }
--- core/src/test/java/tokyo/peya/langjal/compiler/instructions/utils/AbstractInstructionTestCase.java
+++ core/src/test/java/tokyo/peya/langjal/compiler/instructions/utils/AbstractInstructionTestCase.java
@@ -1,17 +1,29 @@
 package tokyo.peya.langjal.compiler.instructions.utils;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.TokenStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import tokyo.peya.langjal.compiler.JALLexer;
+import tokyo.peya.langjal.compiler.JALParser;
 import tokyo.peya.langjal.compiler.instructions.AbstractInstructionEvaluator;
+
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public abstract class AbstractInstructionTestCase
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class AbstractInstructionTestCase<P extends ParserRuleContext, T extends AbstractInstructionEvaluator<P>>
 {
     private final int[] expectedOpCodes;
-    private final AbstractInstructionEvaluator<?> evaluator;
+    private final T evaluator;
 
-    public AbstractInstructionTestCase(AbstractInstructionEvaluator<?> evaluator, int... expectedOpCodes)
+    public AbstractInstructionTestCase(T evaluator, int... expectedOpCodes)
     {
         this.evaluator = evaluator;
         this.expectedOpCodes = expectedOpCodes;
@@ -36,5 +48,35 @@ public abstract class AbstractInstructionTestCase
         }
 
         Assertions.assertEquals(this.expectedOpCodes.length, actualOpCodes.length, "Evaluator accepts unexpected number of opcodes");
+    }
+
+    public abstract String[] getValidInstructions();
+
+    @ParameterizedTest
+    @MethodSource("getValidInstructions")
+    public void testParseValidInstructions(String instruction)
+    {
+        try
+        {
+            parseInstruction(instruction, this.evaluator::map);
+        }
+        catch (Exception e)
+        {
+            Assertions.fail(String.format("Valid instruction '%s' threw an exception: %s", instruction, e.getMessage()));
+        }
+    }
+
+
+    protected static <T extends ParserRuleContext> T parseInstruction(String instruction, Function<? super JALParser.InstructionContext, T> mapper)
+    {
+        return parseSource(instruction, p -> mapper.apply(p.instruction()));
+    }
+
+    protected static <T extends ParserRuleContext> T parseSource(String source, Function<? super JALParser, T> mapper)
+    {
+        JALLexer lexer = new JALLexer(CharStreams.fromString(source));
+        TokenStream tokens = new CommonTokenStream(lexer);
+        JALParser parser = new JALParser(tokens);
+        return mapper.apply(parser);
     }
 }
