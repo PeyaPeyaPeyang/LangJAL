fead181b7c5d59ecc42d5f5ccbfc6b2d4652240a
PeyaPeyaPeyang <peyang@peya.tokyo>
2026-05-11 19:05:39 +0900

    test: Add unit tests for constant loading and local variable loading instructions

---
+++ core/src/test/java/tokyo/peya/langjal/compiler/instructions/TestXConst.java
@@ -0,0 +1,154 @@
+package tokyo.peya.langjal.compiler.instructions;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.junit.jupiter.api.Nested;
+import tokyo.peya.langjal.compiler.JALParser;
+import tokyo.peya.langjal.compiler.instructions.utils.AbstractInstructionTestCase;
+import tokyo.peya.langjal.compiler.instructions.utils.StackMachine;
+import tokyo.peya.langjal.compiler.instructions.xconst.InstructionEvaluatorAConstNull;
+import tokyo.peya.langjal.compiler.instructions.xconst.InstructionEvaluatorDConstN;
+import tokyo.peya.langjal.compiler.instructions.xconst.InstructionEvaluatorFConstN;
+import tokyo.peya.langjal.compiler.instructions.xconst.InstructionEvaluatorIConstN;
+import tokyo.peya.langjal.compiler.instructions.xconst.InstructionEvaluatorLConstN;
+import tokyo.peya.langjal.compiler.jvm.EOpcodes;
+
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.doubleValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.floatValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.integerValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.longValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.nullValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.create;
+
+public abstract class TestXConst<T extends ParserRuleContext, E extends AbstractInstructionEvaluator<T>>
+        extends AbstractInstructionTestCase<T, E>
+{
+    private final StackMachine.StackValue outputValue;
+    private final String syntax;
+    private final int opcode;
+
+    protected TestXConst(E evaluator, int... expectedOpCodes)
+    {
+        super(evaluator, expectedOpCodes);
+        this.outputValue = null;
+        this.syntax = null;
+        this.opcode = -1;
+    }
+
+    protected TestXConst(E evaluator, StackMachine.StackValue outputValue, String syntax, int opcode)
+    {
+        super(evaluator, opcode);
+        this.outputValue = outputValue;
+        this.syntax = syntax;
+        this.opcode = opcode;
+    }
+
+    protected AbstractInstructionTestCase.InstructionCase[] single(StackMachine.StackValue outputValue, String syntax, int opcode)
+    {
+        return set(
+                of(
+                        create().expected(create(outputValue)),
+                        syntax,
+                        opcode
+                )
+        );
+    }
+
+    @Override
+    public AbstractInstructionTestCase.InstructionCase[] getValidInstructionSyntaxes()
+    {
+        if (!(this.outputValue == null || this.syntax == null || this.opcode == -1))
+        {
+            return single(this.outputValue, this.syntax, this.opcode);
+        }
+
+        return set();
+    }
+
+    @Nested
+    static class TestAConstNull extends TestXConst<JALParser.JvmInsAconstNullContext, InstructionEvaluatorAConstNull>
+    {
+        TestAConstNull()
+        {
+            super(new InstructionEvaluatorAConstNull(), nullValue(), "aconst_null", EOpcodes.ACONST_NULL);
+        }
+    }
+
+    @Nested
+    static class TestDConstN extends TestXConst<JALParser.JvmInsDconstNContext, InstructionEvaluatorDConstN>
+    {
+        TestDConstN()
+        {
+            super(new InstructionEvaluatorDConstN(), EOpcodes.DCONST_0, EOpcodes.DCONST_1);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    of(create().expected(create(doubleValue())), "dconst_0", EOpcodes.DCONST_0),
+                    of(create().expected(create(doubleValue())), "dconst_1", EOpcodes.DCONST_1)
+            );
+        }
+    }
+
+    @Nested
+    static class TestFConstN extends TestXConst<JALParser.JvmInsFconstNContext, InstructionEvaluatorFConstN>
+    {
+        TestFConstN()
+        {
+            super(new InstructionEvaluatorFConstN(), EOpcodes.FCONST_0, EOpcodes.FCONST_1, EOpcodes.FCONST_2);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    of(create().expected(create(floatValue())), "fconst_0", EOpcodes.FCONST_0),
+                    of(create().expected(create(floatValue())), "fconst_1", EOpcodes.FCONST_1),
+                    of(create().expected(create(floatValue())), "fconst_2", EOpcodes.FCONST_2)
+            );
+        }
+    }
+
+    @Nested
+    static class TestIConstN extends TestXConst<JALParser.JvmInsIconstNContext, InstructionEvaluatorIConstN>
+    {
+        TestIConstN()
+        {
+            super(new InstructionEvaluatorIConstN(), EOpcodes.ICONST_0, EOpcodes.ICONST_1, EOpcodes.ICONST_2,
+                    EOpcodes.ICONST_3, EOpcodes.ICONST_4, EOpcodes.ICONST_5);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    of(create().expected(create(integerValue())), "iconst_m1", EOpcodes.ICONST_M1),
+                    of(create().expected(create(integerValue())), "iconst_0", EOpcodes.ICONST_0),
+                    of(create().expected(create(integerValue())), "iconst_1", EOpcodes.ICONST_1),
+                    of(create().expected(create(integerValue())), "iconst_2", EOpcodes.ICONST_2),
+                    of(create().expected(create(integerValue())), "iconst_3", EOpcodes.ICONST_3),
+                    of(create().expected(create(integerValue())), "iconst_4", EOpcodes.ICONST_4),
+                    of(create().expected(create(integerValue())), "iconst_5", EOpcodes.ICONST_5)
+            );
+        }
+    }
+
+    @Nested
+    static class TestLConstN extends TestXConst<JALParser.JvmInsLconstNContext, InstructionEvaluatorLConstN>
+    {
+        TestLConstN()
+        {
+            super(new InstructionEvaluatorLConstN(), EOpcodes.LCONST_0, EOpcodes.LCONST_1);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    of(create().expected(create(longValue())), "lconst_0", EOpcodes.LCONST_0),
+                    of(create().expected(create(longValue())), "lconst_1", EOpcodes.LCONST_1)
+            );
+        }
+    }
+}
---
+++ core/src/test/java/tokyo/peya/langjal/compiler/instructions/TestXLoad.java
@@ -0,0 +1,252 @@
+package tokyo.peya.langjal.compiler.instructions;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.junit.jupiter.api.Nested;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.VarInsnNode;
+import tokyo.peya.langjal.compiler.JALParser;
+import tokyo.peya.langjal.compiler.instructions.utils.AbstractInstructionTestCase;
+import tokyo.peya.langjal.compiler.instructions.utils.StackMachine;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorALoad;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorALoadN;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorDLoad;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorDLoadN;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorFLoad;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorFLoadN;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorILoad;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorILoadN;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorLLoad;
+import tokyo.peya.langjal.compiler.instructions.xload.InstructionEvaluatorLLoadN;
+import tokyo.peya.langjal.compiler.jvm.EOpcodes;
+import tokyo.peya.langjal.compiler.jvm.TypeDescriptor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.doubleValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.floatValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.integerValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.longValue;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.StackValues.object;
+import static tokyo.peya.langjal.compiler.instructions.utils.StackMachine.create;
+
+public abstract class TestXLoad<T extends ParserRuleContext, E extends AbstractInstructionEvaluator<T>>
+        extends AbstractInstructionTestCase<T, E>
+{
+    protected TestXLoad(E evaluator, int... expectedOpCodes)
+    {
+        super(evaluator, expectedOpCodes);
+    }
+
+    protected InstructionCase localLoad(int index, StackMachine.StackValue value, String syntax, int opcode)
+    {
+        return of(
+                create().set(index, value).expected(create(value).set(index, value)),
+                syntax,
+                new VarInsnNode(opcode, index)
+        );
+    }
+
+    @Override
+    protected void assertInstructionEquals(AbstractInsnNode expected, AbstractInsnNode actual)
+    {
+        super.assertInstructionEquals(expected, actual);
+
+        VarInsnNode expectedVar = (VarInsnNode) expected;
+        VarInsnNode actualVar = (VarInsnNode) actual;
+        assertEquals(expectedVar.var, actualVar.var, "local variable index does not match");
+    }
+
+    @Nested
+    static class TestALoad extends TestXLoad<JALParser.JvmInsAloadContext, InstructionEvaluatorALoad>
+    {
+        private static final StackMachine.StackValue OBJECT_VALUE = object(TypeDescriptor.parse("LMyClass;"));
+
+        TestALoad()
+        {
+            super(new InstructionEvaluatorALoad(), EOpcodes.ALOAD);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(1, OBJECT_VALUE, "aload 1", Opcodes.ALOAD),
+                    localLoad(300, OBJECT_VALUE, "wide aload 300", Opcodes.ALOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestALoadN extends TestXLoad<JALParser.JvmInsAloadNContext, InstructionEvaluatorALoadN>
+    {
+        private static final StackMachine.StackValue OBJECT_VALUE = object(TypeDescriptor.parse("LMyClass;"));
+
+        TestALoadN()
+        {
+            super(new InstructionEvaluatorALoadN(), EOpcodes.ALOAD_0, EOpcodes.ALOAD_1, EOpcodes.ALOAD_2, EOpcodes.ALOAD_3);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(0, OBJECT_VALUE, "aload_0", Opcodes.ALOAD),
+                    localLoad(1, OBJECT_VALUE, "aload_1", Opcodes.ALOAD),
+                    localLoad(2, OBJECT_VALUE, "aload_2", Opcodes.ALOAD),
+                    localLoad(3, OBJECT_VALUE, "aload_3", Opcodes.ALOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestDLoad extends TestXLoad<JALParser.JvmInsDloadContext, InstructionEvaluatorDLoad>
+    {
+        TestDLoad()
+        {
+            super(new InstructionEvaluatorDLoad(), EOpcodes.DLOAD);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(1, doubleValue(), "dload 1", Opcodes.DLOAD),
+                    localLoad(300, doubleValue(), "wide dload 300", Opcodes.DLOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestDLoadN extends TestXLoad<JALParser.JvmInsDloadNContext, InstructionEvaluatorDLoadN>
+    {
+        TestDLoadN()
+        {
+            super(new InstructionEvaluatorDLoadN(), EOpcodes.DLOAD_0, EOpcodes.DLOAD_1, EOpcodes.DLOAD_2, EOpcodes.DLOAD_3);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(0, doubleValue(), "dload_0", Opcodes.DLOAD),
+                    localLoad(1, doubleValue(), "dload_1", Opcodes.DLOAD),
+                    localLoad(2, doubleValue(), "dload_2", Opcodes.DLOAD),
+                    localLoad(3, doubleValue(), "dload_3", Opcodes.DLOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestFLoad extends TestXLoad<JALParser.JvmInsFloadContext, InstructionEvaluatorFLoad>
+    {
+        TestFLoad()
+        {
+            super(new InstructionEvaluatorFLoad(), EOpcodes.FLOAD);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(1, floatValue(), "fload 1", Opcodes.FLOAD),
+                    localLoad(300, floatValue(), "wide fload 300", Opcodes.FLOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestFLoadN extends TestXLoad<JALParser.JvmInsFloadNContext, InstructionEvaluatorFLoadN>
+    {
+        TestFLoadN()
+        {
+            super(new InstructionEvaluatorFLoadN(), EOpcodes.FLOAD_0, EOpcodes.FLOAD_1, EOpcodes.FLOAD_2, EOpcodes.FLOAD_3);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(0, floatValue(), "fload_0", Opcodes.FLOAD),
+                    localLoad(1, floatValue(), "fload_1", Opcodes.FLOAD),
+                    localLoad(2, floatValue(), "fload_2", Opcodes.FLOAD),
+                    localLoad(3, floatValue(), "fload_3", Opcodes.FLOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestILoad extends TestXLoad<JALParser.JvmInsIloadContext, InstructionEvaluatorILoad>
+    {
+        TestILoad()
+        {
+            super(new InstructionEvaluatorILoad(), EOpcodes.ILOAD);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(1, integerValue(), "iload 1", Opcodes.ILOAD),
+                    localLoad(300, integerValue(), "wide iload 300", Opcodes.ILOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestILoadN extends TestXLoad<JALParser.JvmInsIloadNContext, InstructionEvaluatorILoadN>
+    {
+        TestILoadN()
+        {
+            super(new InstructionEvaluatorILoadN(), EOpcodes.ILOAD_0, EOpcodes.ILOAD_1, EOpcodes.ILOAD_2, EOpcodes.ILOAD_3);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(0, integerValue(), "iload_0", Opcodes.ILOAD),
+                    localLoad(1, integerValue(), "iload_1", Opcodes.ILOAD),
+                    localLoad(2, integerValue(), "iload_2", Opcodes.ILOAD),
+                    localLoad(3, integerValue(), "iload_3", Opcodes.ILOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestLLoad extends TestXLoad<JALParser.JvmInsLloadContext, InstructionEvaluatorLLoad>
+    {
+        TestLLoad()
+        {
+            super(new InstructionEvaluatorLLoad(), EOpcodes.LLOAD);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(1, longValue(), "lload 1", Opcodes.LLOAD),
+                    localLoad(300, longValue(), "wide lload 300", Opcodes.LLOAD)
+            );
+        }
+    }
+
+    @Nested
+    static class TestLLoadN extends TestXLoad<JALParser.JvmInsLloadNContext, InstructionEvaluatorLLoadN>
+    {
+        TestLLoadN()
+        {
+            super(new InstructionEvaluatorLLoadN(), EOpcodes.LLOAD_0, EOpcodes.LLOAD_1, EOpcodes.LLOAD_2, EOpcodes.LLOAD_3);
+        }
+
+        @Override
+        public InstructionCase[] getValidInstructionSyntaxes()
+        {
+            return set(
+                    localLoad(0, longValue(), "lload_0", Opcodes.LLOAD),
+                    localLoad(1, longValue(), "lload_1", Opcodes.LLOAD),
+                    localLoad(2, longValue(), "lload_2", Opcodes.LLOAD),
+                    localLoad(3, longValue(), "lload_3", Opcodes.LLOAD)
+            );
+        }
+    }
+}
